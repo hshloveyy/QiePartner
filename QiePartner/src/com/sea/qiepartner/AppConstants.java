7724c9f1f5692f5356fcbb1c465b77e0591c2a55
hshloveyy <386184368@qq.com>
2016-03-28 17:30:19 +0800

    uiListener

---
+++ QiePartner/src/com/sea/qiepartner/AppConstants.java
@@ -0,0 +1,11 @@
+package com.sea.qiepartner;
+
+public class AppConstants {
+
+	/**
+	 * QQµÇÂ¼APP_ID
+	 */
+	public static final String APP_ID = "1105287282";
+	
+	public static final String TAG = "QiePartner";
+}
